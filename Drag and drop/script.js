d36a98dd4c9f90539f75c1fec523bdd4e0782821
Krish Varma <skullthecoder@gmail.com>
2025-09-05 12:48:50 +0530

    projects

---
+++ Drag and drop/script.js
@@ -0,0 +1,29 @@
+let boxes = document.querySelectorAll(".box");
+
+const rightbox = document.getElementById("right");
+const leftbox = document.getElementById("left");
+let selected = null;
+
+boxes.forEach(box => {
+    box.addEventListener("dragstart", (e) => {
+        selected = e.target;
+
+    });
+
+})
+
+rightbox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+rightbox.addEventListener("drop", (e) => {
+    rightbox.appendChild(selected);
+    selected = null;
+})
+leftbox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+leftbox.addEventListener("drop", (e) => {
+    leftbox.appendChild(selected);
+    selected = null;
+})
+
